ddad26c6f72a3fc4cdbeabda259b5a1f0a294c38
SammyFromBerlin <s.surabov@campus.tu-berlin.de>
2021-12-01 18:22:03 +0100

    Solved First Part of first Day

--- 1-1-Solution.py
+++ 1-1-Solution.py
@@ -1,1 +1,17 @@
-print("hi")
+import os 
+
+with open("1-1.txt") as file:
+    #print(file.readlines())
+    #print(type(file.read()))
+    liste = file.readlines()
+    currEle = int(liste[0])
+    v = 0
+    for i in liste:
+        tmp = int(i)
+        if currEle < tmp :
+            v += 1
+        currEle = tmp
+    print(str(v))
+
+    
+    
