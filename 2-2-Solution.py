d0ecaaa9cb107a92c28142b5754b4c64e11d5c69
SammyFromBerlin <s.surabov@campus.tu-berlin.de>
2021-12-02 20:36:32 +0100

    Part 2 prettier code

--- 2-2-Solution.py
+++ 2-2-Solution.py
@@ -6,18 +6,16 @@ aim = 0
 with open("2.txt") as file:
     list = file.readlines()
     for i in list:
-       if str(i[0]) == "f":
-           tmp = i.split(" ")[1][0]
-           h += int(tmp)
-           d = d + (aim * int(tmp))
-       elif str(i[0]) == "d":
-          tmp = i.split(" ")[1][0] 
-          aim += int(tmp)
-       elif str(i[0]) == "u":
-           tmp = i.split(" ")[1][0]
-           aim -= int(tmp)
-           
-    result = h * d
+        direction = i.split(" ")[0]
+        units = int(i.split(" ")[1])
+        if direction == "forward":
+            h += units
+            d = d + (aim * units )
+        elif direction == "down":
+            aim += units
+        elif direction == "up":
+            aim -= units
+result = h * d
 print(str(h))
 print(str(d))
 print(str(result))
